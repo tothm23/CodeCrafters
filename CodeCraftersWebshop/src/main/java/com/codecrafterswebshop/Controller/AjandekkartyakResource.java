d793fc9fe13074a826479b49bd53cea8f0da095f
tothm23 <tothmilan224@gmail.com>
2023-09-25 16:47:16 +0200

    DELETE ajandekkartyak végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/AjandekkartyakResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/AjandekkartyakResource.java
@@ -4,6 +4,7 @@ import com.codecrafterswebshop.Model.Ajandekkartya;
 import com.codecrafterswebshop.Service.AjandekkartyaService;
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -22,17 +23,17 @@ import javax.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AjandekkartyakResource {
-
+    
     public AjandekkartyakResource() {
     }
-
+    
     @GET
     @Path("{ajandekKartyaId}")
     public List<Ajandekkartya> ajandekKartya(@PathParam("ajandekKartyaId") Integer id) {
         List<Ajandekkartya> result = AjandekkartyaService.ajandekKartya(id);
         return result;
     }
-
+    
     @POST
     public Response ujAjandekKartya(Ajandekkartya a) {
         String result = AjandekkartyaService.ujAjandekKartya(a.getNev(), a.getAr(),
@@ -41,13 +42,21 @@ public class AjandekkartyakResource {
         return Response.status(Response.Status.OK).entity(result)
                 .type(MediaType.APPLICATION_JSON).build();
     }
-
+    
     @PUT
     @Path("{ajandekKartyaId}")
     public Response frissitesAjandekKartya(@PathParam("ajandekKartyaId") Integer id, Ajandekkartya a) {
         String result = AjandekkartyaService.frissitesAjandekKartya(id, a.getNev(), a.getAr(),
                 a.getLeiras(), a.getKep(), a.getAkcio(), a.getMennyisegraktaron(),
                 a.getEszkozId(), a.getPlatformId());
+        return Response.status(Response.Status.OK).entity(result)
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+    
+    @DELETE
+    @Path("{ajandekKartyaId}")
+    public Response torlesAjandekKartya(@PathParam("ajandekKartyaId") Integer id) {
+        String result = AjandekkartyaService.torlesAjandekKartya(id);
         return Response.status(Response.Status.OK).entity(result)
                 .type(MediaType.APPLICATION_JSON).build();
     }
