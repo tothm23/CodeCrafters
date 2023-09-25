ab4ad178753a9d2efc8a27b20d44ad3e3d05d431
tothm23 <tothmilan224@gmail.com>
2023-09-25 16:07:16 +0200

    GET ajandekkartyak/{ajandekKartyaId} végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/AjandekkartyakResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/AjandekkartyakResource.java
@@ -1,6 +1,14 @@
 package com.codecrafterswebshop.Controller;
 
+import com.codecrafterswebshop.Model.Ajandekkartya;
+import com.codecrafterswebshop.Service.AjandekkartyaService;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -8,8 +16,17 @@ import javax.ws.rs.Path;
  * @author tothm23
  */
 @Path("ajandekkartyak")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class AjandekkartyakResource {
 
     public AjandekkartyakResource() {
+    }
+
+    @GET
+    @Path("{ajandekKartyaId}")
+    public List<Ajandekkartya> ajandekKartya(@PathParam("ajandekKartyaId") Integer id) {
+        List<Ajandekkartya> result = AjandekkartyaService.ajandekKartya(id);
+        return result;
     }
 }
--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/AjandekkartyaService.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/AjandekkartyaService.java
@@ -1,9 +1,15 @@
 package com.codecrafterswebshop.Service;
 
+import com.codecrafterswebshop.Model.Ajandekkartya;
+import java.util.List;
+
 /**
  *
  * @author tothm23
  */
 public class AjandekkartyaService {
 
+    public static List<Ajandekkartya> ajandekKartya(Integer ajandekKartyaIdBE) {
+        return Ajandekkartya.ajandekKartya(ajandekKartyaIdBE);
+    }
 }
