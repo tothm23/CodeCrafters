094a31761fe47fe76198a2d430a5d799d0d91b13
tothm23 <tothmilan224@gmail.com>
2023-09-25 16:31:22 +0200

    POST ajandekkartyak végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/AjandekkartyaService.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/AjandekkartyaService.java
@@ -12,4 +12,17 @@ public class AjandekkartyaService {
     public static List<Ajandekkartya> ajandekKartya(Integer ajandekKartyaIdBE) {
         return Ajandekkartya.ajandekKartya(ajandekKartyaIdBE);
     }
+
+    public static String ujAjandekKartya(String nevBE, Integer arBE, String leirasBE,
+            String kepBE, Integer akcioBE, Integer mennyisegraktaronBE, Integer eszkozIdBE, Integer platformIdBE) {
+        try {
+            if (Ajandekkartya.ujAjandekKartya(nevBE, arBE, leirasBE, kepBE, akcioBE, mennyisegraktaronBE, eszkozIdBE, platformIdBE)) {
+                return "Ajándék kártya hozzáadva!";
+            } else {
+                return "Hiba az ajándék kártya hozzáadásánál!";
+            }
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
 }
