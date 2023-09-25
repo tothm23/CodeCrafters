0a4ab091ebd94c7721c02834f340f26ede94621c
tothm23 <tothmilan224@gmail.com>
2023-09-25 14:17:09 +0200

    Ajándék kártya erőforrás létrehozása

---
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package com.codecrafterswebshop.Controller;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author tothm23
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.codecrafterswebshop.Controller.AjandekkartyakResource.class);
+    }
+
+}
