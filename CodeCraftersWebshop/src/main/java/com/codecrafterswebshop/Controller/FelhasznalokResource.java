516223b366b3b1cfaebda9f73b4a47db758604fa
tothm23 <tothmilan224@gmail.com>
2023-10-02 20:27:22 +0200

    GET felhasznalok végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
@@ -1,6 +1,13 @@
 package com.codecrafterswebshop.Controller;
 
+import com.codecrafterswebshop.Model.Felhasznalo;
+import com.codecrafterswebshop.Service.FelhasznaloService;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -8,9 +15,17 @@ import javax.ws.rs.Path;
  * @author tothm23
  */
 @Path("felhasznalok")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class FelhasznalokResource {
 
     public FelhasznalokResource() {
+    }
+
+    @GET
+    public List<Felhasznalo> felhasznalok() {
+        List<Felhasznalo> felhasznalok = FelhasznaloService.felhasznalok();
+        return felhasznalok;
     }
 
 }
