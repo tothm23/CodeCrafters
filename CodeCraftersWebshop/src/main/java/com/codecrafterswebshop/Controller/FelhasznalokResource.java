d04b74b8a932b2dfab78f48226c16fd4151358dc
tothm23 <tothmilan224@gmail.com>
2023-10-03 10:22:49 +0200

    POST felhasznalok végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
@@ -5,9 +5,11 @@ import com.codecrafterswebshop.Service.FelhasznaloService;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -26,6 +28,15 @@ public class FelhasznalokResource {
     public List<Felhasznalo> felhasznalok() {
         List<Felhasznalo> felhasznalok = FelhasznaloService.felhasznalok();
         return felhasznalok;
+    }
+
+    @POST
+    public Response ujFelhasznalo(Felhasznalo f) {
+        String result = FelhasznaloService.ujFelhasznalo(f.getFelhasznaloNev(), f.getVezetekNev(),
+                f.getKeresztNev(), f.getSzuletesiDatum(), f.getEmail(), f.getJelszo(),
+                f.getOrszag(), f.getTelefon());
+        return Response.status(Response.Status.OK).entity(result)
+                .type(MediaType.APPLICATION_JSON).build();
     }
 
 }
