4e1163c9eac9681d3bd316e1fc3d75829d9f70b1
tothm23 <tothmilan224@gmail.com>
2023-10-31 14:32:06 +0100

    GET felhasznalok végpont optimalizálása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
@@ -2,7 +2,6 @@ package com.codecrafterswebshop.Controller;
 
 import com.codecrafterswebshop.Model.Felhasznalo;
 import com.codecrafterswebshop.Service.FelhasznaloService;
-import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -26,9 +26,10 @@ public class FelhasznalokResource {
     }
 
     @GET
-    public List<Felhasznalo> felhasznalok() {
-        List<Felhasznalo> felhasznalok = FelhasznaloService.felhasznalok();
-        return felhasznalok;
+    public Response felhasznalok() {
+        JSONArray eredmeny = FelhasznaloService.felhasznalok();
+        return Response.status(Response.Status.OK).entity(eredmeny.toString())
+                .type(MediaType.APPLICATION_JSON).build();
     }
 
     @POST
