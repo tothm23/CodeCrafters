2aceda4042493dc1ece59dddb9be64091bf49ade
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-06 14:29:34 +0100

    fetch javítás
    inputdata mostmár az értéket adja vissza nem üresen küldi a backendnek

--- bejelentkezés_regisztrációsfeulet/script.js
+++ bejelentkezés_regisztrációsfeulet/script.js
@@ -2,18 +2,7 @@ const formPopup = document.querySelector(".form-popup");
 const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 const regisztracio = document.getElementById('regisztracio');
 
-const felhasz = document.getElementById('felhasz').value;
-const vezeteknev = document.getElementById('vezeteknev').value;
-const keresztnev = document.getElementById('keresztnev').value;
-const datum = document.getElementById('datum').value;
-const email = document.getElementById('email').value;
-const jelszo = document.getElementById('jelszo').value;
-const orszag = document.getElementById('orszag').value;
-const telefonszam = document.getElementById('telefonszam').value;
-
-
 document.body.classList.toggle("show-popup");
-
 
 // Show or hide signup form
 signupLoginLink.forEach(link => {
@@ -23,27 +12,33 @@ signupLoginLink.forEach(link => {
     });
 });
 
-//inputdata
-const inputdata = {
-    felhasznaloNev: felhasz,
-    vezetekNev: vezeteknev,
-    keresztNev: keresztnev,
-    szuletesiDatum: datum,
-    email: email,
-    jelszo: jelszo,
-    orszag: orszag,
-    telefon: telefonszam
-}
-
 // regisztráció fetch 
 regisztracio.addEventListener('click', () => {
-    fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok", {
+    const felhasz = document.getElementById('felhasz').value;
+    const vezeteknev = document.getElementById('vezeteknev').value;
+    const keresztnev = document.getElementById('keresztnev').value;
+    const datum = document.getElementById('datum').value;
+    const email = document.getElementById('email').value;
+    const jelszo = document.getElementById('jelszo').value;
+    const orszag = document.getElementById('orszag').value;
+    const telefonszam = document.getElementById('telefonszam').value;
 
+    const inputdata = {
+        felhasznaloNev: felhasz,
+        vezetekNev: vezeteknev,
+        keresztNev: keresztnev,
+        szuletesiDatum: datum,
+        email: email,
+        jelszo: jelszo,
+        orszag: orszag,
+        telefon: telefonszam
+    };
+
+    fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok", {
         method: "POST",
         body: JSON.stringify(inputdata),
         headers: {
             "Content-type": "application/json; charset=UTF-8",
         },
-
     }).then((response) => response.json());
 });
