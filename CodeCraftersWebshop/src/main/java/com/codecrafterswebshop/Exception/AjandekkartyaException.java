f6c1a057073755046630901cefb74976133dd4dd
tothm23 <tothmilan224@gmail.com>
2023-11-08 10:26:37 +0100

    Ajándék kártya hibakezelő osztály létrehozása

---
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Exception/AjandekkartyaException.java
@@ -0,0 +1,12 @@
+package com.codecrafterswebshop.Exception;
+
+/**
+ *
+ * @author tothm23
+ */
+public class AjandekkartyaException extends Exception {
+
+    public AjandekkartyaException(String hiba) {
+        super(hiba);
+    }
+}
