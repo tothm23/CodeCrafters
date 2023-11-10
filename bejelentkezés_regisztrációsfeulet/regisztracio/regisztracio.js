3a4e8ef4ddc65cde721040e90bcba9c36e8fbddf
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-10 19:10:58 +0100

    alert box színe most már jó és ki írja hogy sikeres e a regisztráció

--- bejelentkezés_regisztrációsfeulet/regisztracio/regisztracio.js
+++ bejelentkezés_regisztrációsfeulet/regisztracio/regisztracio.js
@@ -1,32 +1,25 @@
 const formPopup = document.querySelector(".form-popup");
 const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
-const regisztracio = document.getElementById('regisztracio');
-
-const sikeresElem = document.getElementById("sikeresAlert");
-const regisztraciosForm = document.querySelector("form");
-
-
-document.body.classList.toggle("show-popup");
+const regisztracio = document.getElementById("regisztracio");
 
 document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.toggle("show-popup");
-  
-    const sikeresElem = document.getElementById("sikeresAlert"); // Módosítottuk a változó nevét
+
+    const sikeresElem = document.getElementById("Alert");
     const regisztraciosForm = document.querySelector("form");
-  
-    // regisztráció fetch
-    regisztraciosForm.addEventListener("submit", async (event) => {
-      event.preventDefault(); // Az űrlap alapértelmezett működésének megakadályozása
-  
-    const felhasz = document.getElementById('felhasz').value;
-    const vezeteknev = document.getElementById('vezeteknev').value;
-    const keresztnev = document.getElementById('keresztnev').value;
-    const datum = document.getElementById('datum').value;
-    const email = document.getElementById('email').value;
-    const jelszo = document.getElementById('jelszo').value;
-    const orszag = document.getElementById('orszag').value;
-    const telefonszam = document.getElementById('telefonszam').value;
-  
+
+    regisztraciosForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const felhasz = document.getElementById("felhasz").value;
+      const vezeteknev = document.getElementById("vezeteknev").value;
+      const keresztnev = document.getElementById("keresztnev").value;
+      const datum = document.getElementById("datum").value;
+      const email = document.getElementById("email").value;
+      const jelszo = document.getElementById("jelszo").value;
+      const orszag = document.getElementById("orszag").value;
+      const telefonszam = document.getElementById("telefonszam").value;
+
       const inputdata = {
         felhasznaloNev: felhasz,
         vezetekNev: vezeteknev,
@@ -35,38 +28,45 @@ document.addEventListener("DOMContentLoaded", function () {
         email: email,
         jelszo: jelszo,
         orszag: orszag,
-        telefon: telefonszam
+        telefon: telefonszam,
       };
-  
-      try {
-        const valasz = await fetch(
-          "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok",
-          {
-            method: "POST",
-            body: JSON.stringify(inputdata),
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          }
-        );
-  
-        if (valasz.ok) {
-          const adat = await valasz.json();
-          sikeresElem.textContent = adat.message;
-          sikeresElem.style.display = "block";
-  
-          // Alert üzenet megjelenítése
-          alert("Sikeres: " + adat.message);
-        } else {
-          throw new Error("A regisztráció nem sikerült.");
+      // regisztráció fetch
+      fetch(
+        "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok",
+        {
+          method: "POST",
+          body: JSON.stringify(inputdata),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
         }
-      } catch (hiba) {
-        sikeresElem.textContent = hiba.message;
-        console.error("Hiba:", hiba);
-  
-        // Alert üzenet megjelenítése hiba esetén
-        alert("Hiba: " + hiba.message);
-      }
+      )
+      .then((valasz) => valasz.text())
+      .then((adat) => {
+        if(adat=="Felhasználó hozzáadva!"){
+          sikeresElem.style.color = "green";
+          sikeresElem.style.backgroundColor = "lightgreen";
+          sikeresElem.style.borderColor = "darkgreen";
+          sikeresElem.style.display = "block";
+        }
+        else{
+          sikeresElem.style.color = "red";
+          sikeresElem.style.backgroundColor = "lightcoral";
+          sikeresElem.style.borderColor = "darkred";
+          sikeresElem.style.display = "block";
+        }
+        try {
+          const elemzettAdat = JSON.parse(adat);
+          sikeresElem.innerHTML = elemzettAdat.message;
+        } catch (error) {
+          console.log(error);
+          sikeresElem.innerHTML = adat;
+          
+        }
+      })
+      .catch((hiba) => {
+        sikeresElem.innerHTML = hiba;
+      });
     });
   });
-  
+
