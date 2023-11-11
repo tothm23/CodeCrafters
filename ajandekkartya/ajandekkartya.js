39c624dd0b39e8171fa1246a38ea7ffec8819de5
tothm23 <tothmilan224@gmail.com>
2023-11-11 18:43:45 +0100

    JavaScript hozzáadása

---
+++ ajandekkartya/ajandekkartya.js
@@ -0,0 +1,44 @@
+const link = "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/ajandekkartyak";
+
+const kep = document.getElementById("kep");
+const nev = document.getElementById("nev");
+const eszkoz = document.getElementById("eszkoz");
+const platform = document.getElementById("platform");
+const elerheto = document.getElementById("elerheto");
+const eredeti_ar = document.getElementById("eredeti-ar");
+const akcios_ar = document.getElementById("akcios-ar");
+const leiras = document.getElementById("leiras");
+
+// Kiolvassa a paraméterek az URL-ből
+let parameterek = new URL(document.location).searchParams;
+
+// Kiolvassa az id paramétert
+let id = parseInt(parameterek.get("id"));
+
+// GET kérés
+fetch(`${link}/${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.id) {
+      kep.innerHTML = data.nev;
+      kep.setAttribute("src", data.kep);
+
+      nev.innerHTML = data.nev;
+      eszkoz.innerHTML = data.eszkoz;
+      platform.innerHTML = data.platform;
+
+      if (data.mennyisegraktaron == 0) {
+        elerheto.innerHTML = "elérhető";
+        elerheto.style.color = "#95e72d";
+      } else {
+        elerheto.innerHTML = "nem elérhető";
+        elerheto.style.color = "#ff0000";
+      }
+
+      eredeti_ar.innerHTML = data.ar + " Ft";
+      akcios_ar.innerHTML = Math.round(data.ar - data.akcio / 100) + " Ft";
+      leiras.innerHTML = data.leiras;
+    } else {
+      alert("A játék nem található!");
+    }
+  });
