a792296e0f11b751316e30818bd6714c29731132
tothm23 <tothmilan224@gmail.com>
2023-11-13 18:54:22 +0100

    GET termekek végpont megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/TermekekResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/TermekekResource.java
@@ -1,6 +1,11 @@
 package com.codecrafterswebshop.Controller;
 
+import com.codecrafterswebshop.Service.TermekekService;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
 
 /**
  * REST Web Service
@@ -13,4 +18,10 @@ public class TermekekResource {
     public TermekekResource() {
     }
 
+    @GET
+    public Response termekek() {
+        JSONArray eredmeny = TermekekService.termekek();
+        return Response.status(Response.Status.OK).entity(eredmeny.toString())
+                .type(MediaType.APPLICATION_JSON).build();
+    }
 }
