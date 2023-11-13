9486805d359b6636e2dcec8cea12de08fe790385
tothm23 <tothmilan224@gmail.com>
2023-11-13 14:00:03 +0100

    jatekok végpont átnevezése

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/ApplicationConfig.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/ApplicationConfig.java
@@ -21,7 +21,7 @@ public class ApplicationConfig extends Application {
         resources.add(com.codecrafterswebshop.Config.CorsFilter.class);
         resources.add(com.codecrafterswebshop.Controller.AjandekkartyaResource.class);
         resources.add(com.codecrafterswebshop.Controller.FelhasznalokResource.class);
-        resources.add(com.codecrafterswebshop.Controller.JatekokResource.class);
+        resources.add(com.codecrafterswebshop.Controller.JatekResource.class);
     }
 
 }
