365de05a0818fde8a53bb7102d3fd86c791676c0
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-13 12:07:13 +0100

    formázás

--- frontend/bejeletkezés/bejeletkezes.js
+++ frontend/bejeletkezés/bejeletkezes.js
@@ -1,41 +1,45 @@
 const formPopup = document.querySelector(".form-popup");
 const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
-const regisztracio = document.getElementById('regisztracio');
-const belepes = document.getElementById('belepes');
-const sikeres = document.getElementById('sikeres');
+const sikeres = document.getElementById("sikeres");
 
-document.body.classList.toggle("show-popup");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const belepes = document.getElementById("bejelentkezes");
 
-//bejelentkezés
-belepes.addEventListener('click', (event) => {
+  document.body.classList.toggle("show-popup");
+
+  //bejelentkezés
+  belepes.addEventListener("click", (event) => {
     event.preventDefault();
-    
-    const felhaszNev = document.querySelector('.input-field:nth-child(1) input').value;
-    const jelszo = document.querySelector('.input-field:nth-child(2) input').value;
 
-    fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok/bejelentkezes", {
+    const felhaszNev = document.querySelector("#felhasz").value;
+    const jelszo = document.querySelector("#jelszo").value;
+
+    fetch(
+      "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok/bejelentkezes",
+      {
         method: "GET",
         body: JSON.stringify({
-            felhasznaloNev: felhaszNev,
-            jelszo: jelszo
+          felhasznaloNev: felhaszNev,
+          jelszo: jelszo,
         }),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    .then((response) => {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    )
+      .then((response) => {
         if (response.ok) {
-            // Sikeres bejelentkezés, itt kezelheted a sikeres bejelentkezés eseményét
-            console.log("Sikeres bejelentkezés!");
+          // Sikeres bejelentkezés, itt kezelheted a sikeres bejelentkezés eseményét
+          console.log("Sikeres bejelentkezés!");
+          window.location.href = "../index.html";
         } else {
-            // Sikertelen bejelentkezés, itt kezelheted a sikertelen bejelentkezés eseményét
-            console.error("Sikertelen bejelentkezés!");
+          // Sikertelen bejelentkezés, itt kezelheted a sikertelen bejelentkezés eseményét
+          console.error("Sikertelen bejelentkezés!");
         }
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         // Hibakezelés, ha valami nem működik a kérés során
         console.error("Hiba történt a bejelentkezés során: ", error);
-    });
+      });
+  });
 });
-
-
