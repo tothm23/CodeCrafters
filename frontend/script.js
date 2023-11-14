51bd598e1aee897656d61061927c71b512cfb183
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-14 10:40:29 +0100

    carousel kezdetleges

--- frontend/script.js
+++ frontend/script.js
@@ -19,3 +19,27 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+
+var games = [
+    { title: "Játék 1 címe", price: "$10", description: "Leírás 1", image: "./kepek/jatekok/miles.png" },
+    { title: "Játék 2 címe", price: "$20", description: "Leírás 2", image: "./kepek/jatekok/fc24.jpg" },
+    { title: "Játék 3 címe", price: "$30", description: "Leírás 3", image: "./kepek/jatekok/gta5.jpg" }
+    // További játékadatokat itt adhatsz hozzá
+];
+
+var carouselInner = document.getElementById("gameCarousel");
+
+games.forEach(function(game, index) {
+    var activeClass = index === 0 ? "active" : "";
+    carouselInner.innerHTML += `
+        <div class="carousel-item ${activeClass}">
+            <img src="${game.image}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>${game.title}</h5>
+                <p>Ár: ${game.price} | Leírás: ${game.description}</p>
+                <!-- További adatokat itt jelenítheted meg -->
+            </div>
+        </div>
+    `;
+});
