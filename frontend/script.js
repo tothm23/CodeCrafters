249bef3661531a72e28d67d6f823a308bf4ec140
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-15 13:01:27 +0100

    vissza gomb

--- frontend/script.js
+++ frontend/script.js
@@ -1,75 +1,73 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Eseménykezelő a mynavbar elemekre
-    var myNavbarItems = document.querySelectorAll('.mynavbar .dropdown-item');
+document.addEventListener("DOMContentLoaded", function () {
+  // Eseménykezelő a mynavbar elemekre
+  var myNavbarItems = document.querySelectorAll(".mynavbar .dropdown-item");
 
-    myNavbarItems.forEach(function (navbarItem) {
-        navbarItem.addEventListener('click', function (event) {
-            // Megtaláljuk az id-t a href-ben
-            var targetId = this.getAttribute('id');
+  myNavbarItems.forEach(function (navbarItem) {
+    navbarItem.addEventListener("click", function (event) {
+      // Megtaláljuk az id-t a href-ben
+      var targetId = this.getAttribute("id");
 
-            // Az aktuális kategória azonosítójának mentése az URL-be
-            var url = new URL(window.location.href);
-            url.searchParams.set('currentCategory', targetId);
-            window.history.pushState({}, '', url);
+      // Az aktuális kategória azonosítójának mentése az URL-be
+      var url = new URL(window.location.href);
+      url.searchParams.set("currentCategory", targetId);
+      window.history.pushState({}, "", url);
 
-            // Az oldal betöltése csak a Termékek kategória esetén
-            if (targetId === 'termekLink') {
-                loadProductPage();
-            }
-        });
+      // Az oldal betöltése csak a Termékek kategória esetén
+      if (targetId === "termekLink") {
+        loadProductPage();
+      }
     });
+  });
 
-    function parseURL() {
-        const url = new URL(window.location);
-        const currentCategory = url.searchParams.get('currentCategory');
+  function parseURL() {
+    const url = new URL(window.location);
+    const currentCategory = url.searchParams.get("currentCategory");
 
-        if (currentCategory) {
-            // Az oldal betöltése csak a Termékek kategória esetén
-            if (currentCategory === 'termekLink') {
-                loadProductPage();
-            }
-        }
+    if (currentCategory) {
+      // Az oldal betöltése csak a Termékek kategória esetén
+      if (currentCategory === "termekLink") {
+        loadProductPage();
+      }
     }
+  }
 
-    function updateURL() {
-        const checkboxes = document.querySelectorAll('.form-check-input');
-        const params = [];
+  function updateURL() {
+    const checkboxes = document.querySelectorAll(".form-check-input");
+    const params = [];
 
-        checkboxes.forEach((checkbox) => {
-            if (checkbox.checked) {
-                const linkId = checkbox.parentElement.getAttribute('id');
-                params.push(linkId);
-
-                // Az aktuális kategória azonosítójának mentése az URL-be
-                const url = new URL(window.location.href);
-                url.searchParams.set('currentCategory', linkId);
-                window.history.pushState({}, '', url);
-            }
-        });
-
-        // Oldal betöltése vagy egyéb műveletek a Termékek kategória esetén
-        if (params.includes('termekLink')) {
-            loadProductPage();
-        }
-    }
-
-    function loadProductPage() {
-        // Oldal betöltése vagy egyéb műveletek a Termékek kategória esetén
-        // Például: window.location.href = './termékoldal/termekekoldal.html';
-        console.log('Oldal betöltése: ./termékoldal/termekekoldal.html');
-    }
-
-    const checkboxes = document.querySelectorAll('.form-check-input');
     checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener('change', function () {
-            updateURL();
-        });
+      if (checkbox.checked) {
+        const linkId = checkbox.parentElement.getAttribute("id");
+        params.push(linkId);
+
+        // Az aktuális kategória azonosítójának mentése az URL-be
+        const url = new URL(window.location.href);
+        url.searchParams.set("currentCategory", linkId);
+        window.history.pushState({}, "", url);
+      }
     });
 
-    parseURL();
+    // Oldal betöltése vagy egyéb műveletek a Termékek kategória esetén
+    if (params.includes("termekLink")) {
+      loadProductPage();
+    }
+  }
+
+  function loadProductPage() {
+    // Oldal betöltése vagy egyéb műveletek a Termékek kategória esetén
+    window.location.href = './termékoldal/termekekoldal.html';
+    console.log("Oldal betöltése: ./termékoldal/termekekoldal.html");
+  }
+
+  const checkboxes = document.querySelectorAll(".form-check-input");
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", function () {
+      updateURL();
+    });
+  });
+
+  parseURL();
 });
-
-
 
 //carouselInner
 var games = [
@@ -77,7 +75,7 @@ var games = [
     title: "Játék 1 címe",
     price: "$10",
     description: "Leírás 1",
-    image: "./kepek/jatekok/miles.png",
+    image: "./kepek/jatekok/BF2.webp",
   },
   {
     title: "Játék 2 címe",
@@ -109,3 +107,24 @@ games.forEach(function (game, index) {
         </div>
     `;
 });
+
+let mybutton = document.getElementById("vissza-gomb");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
