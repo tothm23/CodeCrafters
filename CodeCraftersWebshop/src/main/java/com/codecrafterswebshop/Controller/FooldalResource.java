59c0a19e2d65b201904f3148c4175d2a7be862a1
tothm23 <tothmilan224@gmail.com>
2023-11-15 15:30:06 +0100

    GET fooldal erőforrás megvalósítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FooldalResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FooldalResource.java
@@ -1,6 +1,11 @@
 package com.codecrafterswebshop.Controller;
 
+import com.codecrafterswebshop.Service.FooldalService;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
 
 /**
  * REST Web Service
@@ -11,6 +16,13 @@ import javax.ws.rs.Path;
 public class FooldalResource {
 
     public FooldalResource() {
+    }
+
+    @GET
+    public Response _3veletlenjatek() {
+        JSONArray eredmeny = FooldalService._3veletlenjatek();
+        return Response.status(Response.Status.OK).entity(eredmeny.toString())
+                .type(MediaType.APPLICATION_JSON).build();
     }
 
 }
