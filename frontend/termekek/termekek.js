cefeca0b7f684f97d48be7a39134d3ef96c6e263
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-19 18:38:17 +0100

    a kereső ill. az ár szűrés működik de még van mit javítani

--- frontend/termekek/termekek.js
+++ frontend/termekek/termekek.js
@@ -1,238 +1,87 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // GET kérés
+  // HTML elemek kiválasztása
+  const termekekElem = document.getElementById("termekek");
+  const keresoInput = document.querySelector('.navbar input[type="search"]');
+  const minArInput = document.querySelector('.ar input[name="min"]');
+  const maxArInput = document.querySelector('.ar input[name="max"]');
+  const form = document.querySelector(".navbar form");
+
+  // Eredeti adatok tárolása
+  let eredetiAdatok;
+
+  // Termékek lekérése a szerverről
   fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/termekek")
     .then((valasz) => valasz.json())
     .then((adat) => {
-      var termekek = document.getElementById("termekek");
+      eredetiAdatok = adat;
 
-      for (let i = 0; i < adat.length / 2; i++) {
-        termekek.innerHTML += `
-          <div class="card my-4">
-              <img src="../kepek/ajandekkartya/${adat[i].kep}" class="card-img-top w-50" alt="${adat[i].nev}">
-                  
-              <div class="card-body">
-                  <h5 class="card-title">${adat[i].nev}</h5>
-                  <p class="card-text ar">${adat[i].ar} Ft</p>
-                  <p class="card-text akciosar">${Math.round(adat[i].ar - (adat[i].ar / 100) * adat[i].akcio)}Ft</p>
-                  <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
-          </div>
-      `;
-      }
+      // Kezdeti terméklista megjelenítése
+      termekekMegjelenitese(adat);
 
-      for (let i = adat.length / 2; i < adat.length; i++) {
-        termekek.innerHTML += `
-            <div class="card my-4">
-                <img src="../kepek/jatekok/${adat[i].kep}" class="card-img-top w-50" alt="${adat[i].nev}">
-                    
-                <div class="card-body">
-                    <h5 class="card-title">${adat[i].nev}</h5>
-                    <p class="card-text ar">${adat[i].ar} Ft</p>
-                    <p class="card-text akciosar">${Math.round(adat[i].ar - (adat[i].ar / 100) * adat[i].akcio)}Ft</p>
-                    <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
-            </div>
-        `;
-      }
+      // Eseményfigyelők hozzáadása a keresőhöz és az ármezőkhöz
+      form.addEventListener("submit", function (esemeny) {
+        esemeny.preventDefault();
+        szurokAlkalmazasa();
+      });
+
+      minArInput.addEventListener("input", szurokAlkalmazasa);
+      maxArInput.addEventListener("input", szurokAlkalmazasa);
     })
     .catch((hiba) => alert(hiba));
 
-  var termekekHTML = "";
-  adatok.jatekok.forEach(function (jatek) {
-    termekekHTML += `
-        <div class="termekjatek">
-          <img src="../kepek/${jatek.kepBE}" alt="${jatek.nevBE}">
-          <div class="adatok">
-              <div class="nev">${jatek.nevBE}</div>
-              <div class="ar">${jatek.arBE} HUF</div>
-              <div class="leiras">${jatek.leirasBE}</div>
-              ${jatek.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
+  // Termékek megjelenítése a HTML-ben
+  function termekekMegjelenitese(adatok) {
+    termekekElem.innerHTML = "";
+
+    for (let i = 0; i < adatok.length / 2; i++) {
+      termekekElem.innerHTML += `
+          <div class="card my-4">
+              <img src="../kepek/ajandekkartya/${adatok[i].kep}" class="card-img-top w-50" alt="${adatok[i].nev}">
+              <div class="card-body">
+                  <h5 class="card-title">${adatok[i].nev}</h5>
+                  <p class="card-text ar">${adatok[i].ar} Ft</p>
+                  <p class="card-text akciosar">${Math.round(
+                    adatok[i].ar - (adatok[i].ar / 100) * adatok[i].akcio
+                  )} Ft</p>
+                  <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
+              </div>
           </div>
-        </div>
       `;
-  });
+    }
 
-  adatok.ajandekkartyak.forEach(function (kartya) {
-    termekekHTML += `
-        <div class="termekkartyak">
-          <img src="../kepek/${kartya.kepBE}" alt="${kartya.nevBE}">
-          <div class="adatok">
-              <div class="nev">${kartya.nevBE}</div>
-              <div class="ar">${kartya.arBE} HUF</div>
-              <div class="leiras">${kartya.leirasBE}</div>
-              ${kartya.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
+    for (let i = adatok.length / 2; i < adatok.length; i++) {
+      termekekElem.innerHTML += `
+          <div class="card my-4">
+              <img src="../kepek/jatekok/${adatok[i].kep}" class="card-img-top w-50" alt="${adatok[i].nev}">
+              <div class="card-body">
+                  <h5 class="card-title">${adatok[i].nev}</h5>
+                  <p class="card-text ar">${adatok[i].ar} Ft</p>
+                  <p class="card-text akciosar">${Math.round(
+                    adatok[i].ar - (adatok[i].ar / 100) * adatok[i].akcio
+                  )} Ft</p>
+                  <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
+              </div>
           </div>
-        </div>
       `;
-  });
-  var termekekDiv = document.querySelector(".termekek");
-  termekekDiv.innerHTML = termekekHTML;
-
-  // Keresési mező eseményfigyelése
-  var searchInput = document.querySelector('input[name="search"]');
-  searchInput.addEventListener("input", updateProducts);
-
-  function updateProducts() {
-    var searchTerm = searchInput.value.toLowerCase();
-
-    var filteredProducts = adatok.jatekok.filter(function (jatek) {
-      return (
-        jatek.nevBE.toLowerCase().includes(searchTerm) ||
-        jatek.leirasBE.toLowerCase().includes(searchTerm)
-      );
-    });
-
-    var filteredKartyak = adatok.ajandekkartyak.filter(function (kartya) {
-      return (
-        kartya.nevBE.toLowerCase().includes(searchTerm) ||
-        kartya.leirasBE.toLowerCase().includes(searchTerm)
-      );
-    });
-
-    var filteredHTML = "";
-    filteredProducts.forEach(function (jatek) {
-      filteredHTML += `
-        <div class="termekjatek">
-          <img src="../kepek/${jatek.kepBE}" alt="${jatek.nevBE}">
-          <div class="adatok">
-              <div class="nev">${jatek.nevBE}</div>
-              <div class="ar">${jatek.arBE} HUF</div>
-              <div class="leiras">${jatek.leirasBE}</div>
-              ${jatek.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
-          </div>
-        </div>
-      `;
-    });
-
-    filteredKartyak.forEach(function (kartya) {
-      filteredHTML += `
-        <div class="termekkartyak">
-          <img src="../kepek/${kartya.kepBE}" alt="${kartya.nevBE}">
-          <div class="adatok">
-              <div class="nev">${kartya.nevBE}</div>
-              <div class="ar">${kartya.arBE} HUF</div>
-              <div class="leiras">${kartya.leirasBE}</div>
-              ${kartya.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
-          </div>
-        </div>
-      `;
-    });
-
-    var termekekDiv = document.querySelector(".termekek");
-    termekekDiv.innerHTML = filteredHTML;
+    }
   }
 
-  /*ár kereső*/
-  var minPriceInput = document.querySelector('input[name="min"]');
-  var maxPriceInput = document.querySelector('input[name="max"]');
+  // Szűrők alkalmazása és terméklista frissítése
+  function szurokAlkalmazasa() {
+    const keresendoSzoveg = keresoInput.value.toLowerCase();
+    const minAr = parseFloat(minArInput.value) || 0;
+    const maxAr = parseFloat(maxArInput.value) || Infinity;
 
-  minPriceInput.addEventListener("input", updateProducts);
-  maxPriceInput.addEventListener("input", updateProducts);
+    // Szűrt adatok kiválasztása az eredeti adatok közül
+    const szurtAdatok = eredetiAdatok.filter(
+      (elem) =>
+        (elem.nev.toLowerCase().includes(keresendoSzoveg) ||
+          keresendoSzoveg.includes(elem.nev.toLowerCase())) &&
+        elem.ar >= minAr &&
+        elem.ar <= maxAr
+    );
 
-  function updateProducts() {
-    var minPrice = parseFloat(minPriceInput.value) || 0;
-    var maxPrice = parseFloat(maxPriceInput.value) || Number.MAX_VALUE;
-
-    var filteredProducts = adatok.jatekok.filter(function (jatek) {
-      return jatek.arBE >= minPrice && jatek.arBE <= maxPrice;
-    });
-
-    var filteredKartyak = adatok.ajandekkartyak.filter(function (kartya) {
-      return kartya.arBE >= minPrice && kartya.arBE <= maxPrice;
-    });
-
-    var filteredHTML = "";
-    filteredProducts.forEach(function (jatek) {
-      filteredHTML += `
-                <div class="termekjatek">
-                    <img src="../kepek/${jatek.kepBE}" alt="${jatek.nevBE}">
-                    <div class="adatok">
-                        <div class="nev">${jatek.nevBE}</div>
-                        <div class="ar">${jatek.arBE} HUF</div>
-                        <div class="leiras">${jatek.leirasBE}</div>
-                        ${jatek.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
-                    </div>
-                </div>
-            `;
-    });
-
-    filteredKartyak.forEach(function (kartya) {
-      filteredHTML += `
-                <div class="termekkartyak">
-                    <img src="../kepek/${kartya.kepBE}" alt="${kartya.nevBE}">
-                    <div class="adatok">
-                        <div class="nev">${kartya.nevBE}</div>
-                        <div class="ar">${kartya.arBE} HUF</div>
-                        <div class="leiras">${kartya.leirasBE}</div>
-                        ${kartya.akcioBE ? '<div class="akcio">Akcióban!</div>' : ""}
-                    </div>
-                </div>
-            `;
-    });
-
-    var termekekDiv = document.querySelector(".termekek");
-    termekekDiv.innerHTML = filteredHTML;
-  }
-});
-
-//checkbox manipulálása a navbárból
-document.addEventListener("DOMContentLoaded", function () {
-  // Eseménykezelő a mynavbar elemekre
-  var myNavbarItems = document.querySelectorAll(".mynavbar .gomblink");
-
-  myNavbarItems.forEach(function (navbarItem) {
-    navbarItem.addEventListener("click", function (event) {
-      // Megtaláljuk az id-t a href-ben
-      var targetId = this.getAttribute("id");
-
-      // Megtaláljuk a megfelelő checkboxot
-      var checkboxId = targetId + "input";
-      var checkbox = document.getElementById(checkboxId);
-
-      // Ellenőrizzük, hogy a checkbox megtalálható-e
-      if (checkbox) {
-        // Beállítjuk a checkbox állapotát (kipipáljuk)
-        checkbox.checked = true;
-      }
-    });
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Eseménykezelő a mynavbar elemekre
-  var myNavbarItems = document.querySelectorAll(".mynavbar .gomblink");
-
-  myNavbarItems.forEach(function (navbarItem) {
-    navbarItem.addEventListener("click", function (event) {
-      // Megtaláljuk az id-t a href-ben
-      var targetId = this.getAttribute("id");
-
-      // Megtaláljuk a megfelelő checkboxot
-      var checkboxId = targetId + "input";
-      var checkbox = document.getElementById(checkboxId);
-
-      // Ellenőrizzük, hogy a checkbox megtalálható-e
-      if (checkbox) {
-        // Beállítjuk a checkbox állapotát (kipipáljuk)
-        checkbox.checked = true;
-
-        // Frissítjük az URL-t a kipipált checkbox alapján
-        updateURL();
-      }
-    });
-  });
-
-  // Az alábbi részt hozzáadva az URL frissítése a kipipált checkboxok alapján
-  function updateURL() {
-    const checkboxes = document.querySelectorAll(".form-check-input");
-    const params = [];
-
-    checkboxes.forEach((checkbox) => {
-      if (checkbox.checked) {
-        params.push(checkbox.id.replace("input", ""));
-      }
-    });
-
-    const url = new URL("../termekek/termekek.html", window.location.origin);
-    url.searchParams.set("categories", params.join("re"));
-    window.history.pushState({}, "", url);
+    // Szűrt termékek megjelenítése
+    termekekMegjelenitese(szurtAdatok);
   }
 });
