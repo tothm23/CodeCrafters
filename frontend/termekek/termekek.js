6f1ff851dc8078a5f3f7cc3afe95c464076afc94
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-19 20:53:58 +0100

    checkboxok manipulálása újrara kell írni

--- frontend/termekek/termekek.js
+++ frontend/termekek/termekek.js
@@ -84,4 +84,57 @@ document.addEventListener("DOMContentLoaded", function () {
     // Szűrt termékek megjelenítése
     termekekMegjelenitese(szurtAdatok);
   }
+/*
+  //checkbox manipulálása a navbárból
+  // Eseménykezelő a mynavbar elemekre
+
+  var myNavbarItems = document.querySelectorAll(".mynavbar .gomblink");
+  myNavbarItems.forEach(function (navbarItem) {
+    navbarItem.addEventListener("click", function (event) {
+      // Megtaláljuk az id-t a href-ben
+      var targetId = this.getAttribute("id");
+      // Megtaláljuk a megfelelő checkboxot
+      var checkboxId = targetId + "input";
+      var checkbox = document.getElementById(checkboxId);
+      // Ellenőrizzük, hogy a checkbox megtalálható-e
+      if (checkbox) {
+        // Beállítjuk a checkbox állapotát (kipipáljuk)
+        checkbox.checked = true;
+      }
+    });
+  });
+});
+
+  // Eseménykezelő a mynavbar elemekre
+  var myNavbarItems = document.querySelectorAll(".mynavbar .gomblink");
+  myNavbarItems.forEach(function (navbarItem) {
+    navbarItem.addEventListener("click", function (event) {
+      // Megtaláljuk az id-t a href-ben
+      var targetId = this.getAttribute("id");
+      // Megtaláljuk a megfelelő checkboxot
+      var checkboxId = targetId + "input";
+      var checkbox = document.getElementById(checkboxId);
+      // Ellenőrizzük, hogy a checkbox megtalálható-e
+      if (checkbox) {
+        // Beállítjuk a checkbox állapotát (kipipáljuk)
+        checkbox.checked = true;
+        // Frissítjük az URL-t a kipipált checkbox alapján
+        updateURL();
+      }
+    });
+  // Az alábbi részt hozzáadva az URL frissítése a kipipált checkboxok alapján
+  function updateURL() {
+    const checkboxes = document.querySelectorAll(".form-check-input");
+    const params = [];
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) {
+        params.push(checkbox.id.replace("input", ""));
+      }
+    });
+    const url = new URL("../termekek/termekek.html", window.location.origin);
+    url.searchParams.set("categories", params.join("re"));
+    window.history.pushState({}, "", url);
+  };
+
+*/
 });
