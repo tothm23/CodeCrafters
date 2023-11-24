f2b64248d259d8e427fe5e7fd1753e54f102f4ce
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-11-24 19:21:04 +0100

    főoldal carusel kész

--- frontend/script.js
+++ frontend/script.js
@@ -70,25 +70,42 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // GET kérés
-fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/fooldal")
+fetch(
+  "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/fooldal"
+)
   .then((valasz) => valasz.json())
   .then((adat) => {
     var carouselInner = document.getElementById("gameCarousel");
 
     for (let i = 0; i < adat.length; i++) {
+      var akciosAr = Math.round(
+        adat[i].ar - (adat[i].ar / 100) * adat[i].akcio
+      );
+
       carouselInner.innerHTML += `
-      <div class="carousel-item">
+        <div class="carousel-item">
           <div class="card ">
-              <img src="./kepek/jatekok/${adat[i].kep}" class="d-block w-100" alt="${adat[i].nev}">
+            <img src="./kepek/jatekok/${
+              adat[i].kep
+            }" class="d-block w-100" alt="${adat[i].nev}">
                   
-              <div class="card-body">
-                  <h5 class="card-title">${adat[i].nev}</h5>
-                  <p class="card-text ar">${adat[i].ar} Ft</p>
-                  <p class="card-text akciosar">${Math.round(adat[i].ar - (adat[i].ar / 100) * adat[i].akcio)}Ft</p>
-                  <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
-            </div>    
-      </div>
-    `;
+            <div class="card-body">
+              <h5 class="card-title">${adat[i].nev}</h5>
+              ${
+                adat[i].akcio > 0
+                  ? `<p class="card-text ar" style="text-decoration: line-through;">${adat[i].ar} Ft</p>`
+                  : `<p class="card-text ar">${adat[i].ar} Ft</p>`
+              }
+              ${
+                adat[i].akcio > 0
+                  ? `<p class="card-text akciosar">${akciosAr} Ft</p>`
+                  : ""
+              }
+              <input id="hozzadas" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />
+            </div>  
+          </div>
+        </div>
+      `;
 
       // Kiválasztjuk az első carousel-item-et
       var elsoCarouselElem = document.querySelector(".carousel-item");
@@ -101,8 +118,8 @@ fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/foold
 
 //kosar gomb
 let dbszam = document.getElementsByClassName("darabszam");
-var hozzadasButton = document.getElementById('hozzadas');
-hozzadasButton.addEventListener('click', function () {
+var hozzadasButton = document.getElementById("hozzadas");
+hozzadasButton.addEventListener("click", function () {
   dbszam.style.display = block;
   dbszam.innerHTML = parseInt(dbszam.innerHTML) + 1;
 });
