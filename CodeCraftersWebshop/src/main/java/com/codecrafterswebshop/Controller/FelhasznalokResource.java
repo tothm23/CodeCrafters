e25ed0915670b69b961b983d7f027935d303d333
tothm23 <tothmilan224@gmail.com>
2023-11-25 11:14:59 +0100

    POST bejelentkezes végpont HTTP metódusának javítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/FelhasznalokResource.java
@@ -3,7 +3,6 @@ package com.codecrafterswebshop.Controller;
 import com.codecrafterswebshop.Model.Felhasznalo;
 import com.codecrafterswebshop.Service.FelhasznaloService;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -32,7 +31,7 @@ public class FelhasznalokResource {
                 .type(MediaType.APPLICATION_JSON).build();
     }
 
-    @GET
+    @POST
     @Path("bejelentkezes")
     public Response felhasznaloBelepes(Felhasznalo f) {
         JSONObject eredmeny = FelhasznaloService.felhasznaloBelepes(f.getFelhasznaloNev(), f.getJelszo());
