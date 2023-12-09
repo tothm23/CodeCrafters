ff45132d9ea52f83068269616424a157cf3d5947
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-12-09 15:27:15 +0100

    csak ha egyeznek a jelszavak akkor enged regisztrálni

--- frontend/regisztracio/regisztracio.js
+++ frontend/regisztracio/regisztracio.js
@@ -3,18 +3,22 @@ const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 const regisztracio = document.getElementById("regisztracio");
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.body.classList.toggle("show-popup");
+  document.body.classList.toggle("show-popup");
 
-    const sikeresElem = document.getElementById("Alert");
-    const regisztraciosForm = document.querySelector("form");
+  const regisztraciosForm = document.querySelector("form");
 
-    regisztraciosForm.addEventListener("submit", function (event) {
-      event.preventDefault();
-      const felhasz = document.getElementById("felhasz").value;
-      const vezeteknev = document.getElementById("vezeteknev").value;
-      const keresztnev = document.getElementById("keresztnev").value;
-      const email = document.getElementById("email").value;
-      const jelszo = document.getElementById("jelszo").value;
+  regisztraciosForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const felhasz = document.getElementById("felhasz").value;
+    const vezeteknev = document.getElementById("vezeteknev").value;
+    const keresztnev = document.getElementById("keresztnev").value;
+    const email = document.getElementById("email").value;
+    const jelszo = document.getElementById("jelszo").value;
+    const jelszoujra = document.getElementById("jelszoegyezes").value;
+
+    if (jelszo != jelszoujra) {
+      alert("A jelszavak nem egyeznek!");
+    } else {
 
       const inputdata = {
         felhasznaloNev: felhasz,
@@ -28,27 +32,28 @@ document.addEventListener("DOMContentLoaded", function () {
       };
       // regisztráció fetch
       fetch(
-        "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok",
-        {
-          method: "POST",
-          body: JSON.stringify(inputdata),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8",
-          },
-        }
-      )
-      .then((valasz) => {
-        console.log('Válasz érkezett:', valasz);
-        return valasz.text();
-      })
-      .then((adat) => {
-        alert('Sikeres kérés! Válasz: ' + adat); // Sikeres kérés!
-        console.log('Adatok érkeztek:', adat);
-      })
-      .catch((hiba) => {
-        alert('Hiba történt a kérés során: ' + hiba); // Alert hiba
-        console.error('Hiba:', hiba);
-      });
-    });
-  });
+          "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalok", {
+            method: "POST",
+            body: JSON.stringify(inputdata),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          }
+        )
+        .then((valasz) => {
+          console.log('Válasz érkezett:', valasz);
+          return valasz.text();
+        })
+        .then((adat) => {
+          alert('Sikeres kérés! Válasz: ' + adat); // Sikeres kérés!
+          console.log('Adatok érkeztek:', adat);
+        })
+        .catch((hiba) => {
+          alert('Hiba történt a kérés során: ' + hiba); // Alert hiba
+          console.error('Hiba:', hiba);
+        });
+    }
 
+
+  });
+});
