6d800245423afe854cdcd055de119d72d88dfeb6
varadib02 <91455828+varadib02@users.noreply.github.com>
2023-12-14 22:04:11 +0100

    felhasználó oldal folytatása
    betöltjük a local storage-ból az adatokat és beillesszük az inputba placeholder-nek vagy a felhasználó mint a felhasználó fülön itt szintén láthatja nem csak a felhasználó nevét hanem a saját nevét is

--- frontend/felhasznalo/felhasznalo.js
+++ frontend/felhasznalo/felhasznalo.js
@@ -1,18 +1,76 @@
-const felhaszadatai= document.getElementById('felhasznalo-adatai');
+const felhaszadatai = document.getElementById('felhasznalo-adatai');
+const form_inner = document.getElementById('forminner');
 const bejelentkezettFelhasznalo = JSON.parse(localStorage.getItem("bejelentkezes"));
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
+    adatok_innerHtml();
+    const jelszo = document.getElementById("jelszo").value;
+    const jelszoujra = document.getElementById("jelszoegyezes").value;
+    //menti a módósításokat
+    document.getElementById("mentes").addEventListener("submit", function (event) {
+        event.preventDefault();
+    if (jelszo != jelszoujra) {
+        alert("A jelszavak nem egyeznek!");
+    }
+    else{
+    }
+    });
+    //törli a felhasználót
+    document.getElementById("torles").addEventListener("submit", function (event) {
+        event.preventDefault();
+    if (jelszo != jelszoujra) {
+        alert("A jelszavak nem egyeznek!");
+    }
+    else{
+    }
+    });
+});
 
-    felhaszadatai.innerHTML+=
-    `
+function adatok_innerHtml() {
+    felhaszadatai.innerHTML +=
+        `
     <div class="felhasznalo-kep">
-                    <img src="../kepek/felhasznalo.png" alt="Profil-kep">
-                </div>
-                <div class="felhasznalo-nev">Mákosguba1</div>
-                <div class="felhasznalo-valos-nevek">
-                    <div class="felhasznalo-vezeteknev">Gipsz</div>
-                    <div class="felhasznalo-keresztnev">Elek</div>
-                </div>
+                <img src="../kepek/felhasznalo.png" alt="Profil-kep">
+            </div>
+            <div id="felhasznalo-nev">${bejelentkezettFelhasznalo.felhasznaloNev}</div>
+            <div class="felhasznalo-valos-nevek d-flex flex-row">
+                <div id="felhasznalo-vezeteknev">${bejelentkezettFelhasznalo.vezetekNev}</div>
+                <div id="felhasznalo-keresztnev">${bejelentkezettFelhasznalo.keresztNev}</div>
+            </div>
     `;
 
-});
+    form_inner.innerHTML += `
+                <form>
+                    <div class="mb-3">
+                        <label for="felhasz" class="form-label">Felhasználónév</label>
+                        <input type="text" class="form-control" id="felhasz" placeholder="${bejelentkezettFelhasznalo.felhasznaloNev}" required>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-md-6 mb-3">
+                            <label for="vezeteknev" class="form-label">Vezetéknév</label>
+                            <input type="text" class="form-control" id="vezeteknev" placeholder="${bejelentkezettFelhasznalo.vezetekNev}" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="keresztnev" class="form-label">Keresztnév</label>
+                            <input type="text" class="form-control" id="keresztnev" placeholder="${bejelentkezettFelhasznalo.keresztNev}" required>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" placeholder="${bejelentkezettFelhasznalo.email}" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="jelszo" class="form-label">Jelszó</label>
+                        <input type="password" class="form-control" id="jelszo" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="jelszoegyezes" class="form-label">Jelszó Újra</label>
+                        <input type="password" class="form-control" id="jelszoegyezes" required>
+                    </div>
+                    <div class="felhasznalo-btns">
+                        <button class="btn btn-primary w-100 mt-3 mx-auto" id="mentes" type="submit">Mentés</button>
+                        <button class="btn btn-danger w-100 mt-3 mx-auto" id="torles" type="submit">Törlés</button>
+                    </div>
+                </form>
+                `;
+};
