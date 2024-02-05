6157bf79a5f14df387427da0d2e24b288be601e4
varadib02 <91455828+varadib02@users.noreply.github.com>
2024-02-05 12:34:22 +0100

    user fetch javítása

--- frontend/user/user.js
+++ frontend/user/user.js
@@ -27,10 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 var myHeaders = new Headers();
                 myHeaders.append("Content-Type", "application/json");
                 var raw = JSON.stringify({
-                    "felhasznaloNev": userNameIn,
-                    "vezetekNev": lastNameIn,
-                    "keresztNev": firstNameIn,
-                    "jelszo": pas
+                    "userName": userNameIn,
+                    "lastName": lastNameIn,
+                    "firstName": firstNameIn,
+                    "password": pas
                 });
 
                 var requestOptions = {
@@ -40,7 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     redirect: 'follow'
                 };
 
-                fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalo/${loged_userdata.id}`, requestOptions)
+                fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/user/${loged_userdata.id}`, requestOptions)
                     .then(response => response.text())
                     .then(result => console.log(result))
                     .catch(error => console.log('error', error));
@@ -48,12 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 alert("Felhasználó frissítve");
 
                 let responseObject = {
-                    "felhasznaloNev": userNameIn,
+                    "userName": userNameIn,
                     "email": loged_userdata.email,
                     "id": loged_userdata.id,
-                    "vezetekNev": lastNameIn,
-                    "keresztNev": firstNameIn,
-                    "jelszo": pas
+                    "lastName": lastNameIn,
+                    "firstName": firstNameIn,
+                    "password": pas
                 }
                 loged_userdata = localStorage.setItem("loged_userdata", JSON.stringify(responseObject));
                 console.log(loged_userdata);
@@ -66,10 +66,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 myHeaders.append("Content-Type", "application/json");
 
                 var raw = JSON.stringify({
-                    "felhasznaloNev": userNameIn,
-                    "vezetekNev": lastNameIn,
-                    "keresztNev": firstNameIn,
-                    "jelszo": pas
+                    "userName": userNameIn,
+                    "lastName": lastNameIn,
+                    "firstName": firstNameIn,
+                    "password": pas
                 });
 
                 var requestOptions = {
@@ -79,7 +79,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     redirect: 'follow'
                 };
 
-                fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/felhasznalo/${loged_userdata.id}`, requestOptions)
+                fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/user/${loged_userdata.id}`, requestOptions)
                     .then(response => response.text())
                     .then(result => console.log(result))
                     .catch(error => console.log('error', error));
@@ -99,10 +99,10 @@ function data_innerHtml() {
     <div class="user-img">
                 <img src="../img/user.png" alt="Profil-kep">
             </div>
-            <div id="user-name">${loged_userdata.felhasznaloNev}</div>
+            <div id="user-name">${loged_userdata.userName}</div>
             <div class="d-flex flex-row">
-                <div id="lastName">${loged_userdata.vezetekNev}</div>
-                <div id="fistName">${loged_userdata.keresztNev}</div>
+                <div id="lastName">${loged_userdata.lastName}</div>
+                <div id="fistName">${loged_userdata.firstName}</div>
             </div>
     `;
 
@@ -110,16 +110,16 @@ function data_innerHtml() {
                 <form>
                     <div class="mb-3">
                         <label for="userName" class="form-label">Felhasználónév</label>
-                        <input type="text" class="form-control" id="userName" placeholder="${loged_userdata.felhasznaloNev}" required>
+                        <input type="text" class="form-control" id="userName" placeholder="${loged_userdata.userName}" required>
                     </div>
                     <div class="row mb-3">
                         <div class="col-md-6 mb-3">
                             <label for="lastName" class="form-label">Vezetéknév</label>
-                            <input type="text" class="form-control" id="lastName" placeholder="${loged_userdata.vezetekNev}" required>
+                            <input type="text" class="form-control" id="lastName" placeholder="${loged_userdata.lastName}" required>
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="firstName" class="form-label">Keresztnév</label>
-                            <input type="text" class="form-control" id="firstName" placeholder="${loged_userdata.keresztNev}" required>
+                            <input type="text" class="form-control" id="firstName" placeholder="${loged_userdata.firstName}" required>
                         </div>
                     </div>
                     <div class="mb-3">
