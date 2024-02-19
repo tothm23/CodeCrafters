a4f690138180e930a7739cfd5ffa6aa7f62b37ad
tothm23 <tothmilan224@gmail.com>
2024-02-19 13:11:34 +0100

    Játék POST, PUT, DELETE végpontok visszaállítása

--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/GameResource.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Controller/GameResource.java
@@ -1,8 +1,12 @@
 package com.codecrafterswebshop.Controller;
 
+import com.codecrafterswebshop.Model.Game;
 import com.codecrafterswebshop.Service.GameService;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,6 +29,43 @@ public class GameResource {
     public Response game(@PathParam("gameId") Integer id) {
         JSONObject result = GameService.game(id);
         return Response.status(Response.Status.OK).entity(result.toString())
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @POST
+    public Response newGame(Game g) {
+        String result = GameService.newGame(g.getName(), g.getPrice(),
+                g.getDescription(), g.getImage(), g.getAgeLimit(),
+                g.getDiscount(), g.getInStock(),
+                g.getDeviceId(), g.getPlatformId()
+        );
+        return Response.status(Response.Status.CREATED).entity(result)
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @PUT
+    @Path("{gameId}")
+    public Response updateGame(Game g, @PathParam("gameId") Integer id) {
+        JSONObject result = GameService.game(id);
+        String update = GameService.updateGame(id, g.getName(), g.getPrice(),
+                g.getDescription(), g.getImage(), g.getAgeLimit(),
+                g.getDiscount(), g.getInStock(),
+                g.getDeviceId(), g.getPlatformId()
+        );
+
+        return result.length() == 0 ? Response.status(Response.Status.NOT_FOUND).entity(update)
+                .type(MediaType.APPLICATION_JSON).build() : Response.status(Response.Status.OK).entity(update)
+                .type(MediaType.APPLICATION_JSON).build();
+    }
+
+    @DELETE
+    @Path("{gameId}")
+    public Response deleteGame(@PathParam("gameId") Integer id) {
+        JSONObject result = GameService.game(id);
+        String delete = GameService.deleteGame(id);
+
+        return result.length() == 0 ? Response.status(Response.Status.NOT_FOUND).entity(delete)
+                .type(MediaType.APPLICATION_JSON).build() : Response.status(Response.Status.OK).entity(delete)
                 .type(MediaType.APPLICATION_JSON).build();
     }
 }
--- CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/GameService.java
+++ CodeCraftersWebshop/src/main/java/com/codecrafterswebshop/Service/GameService.java
@@ -1,5 +1,6 @@
 package com.codecrafterswebshop.Service;
 
+import com.codecrafterswebshop.Exception.GameException;
 import com.codecrafterswebshop.Model.Game;
 import java.util.Map;
 import org.json.JSONObject;
@@ -19,6 +20,87 @@ public class GameService {
         }
 
         return obj;
+    }
+
+    public static String newGame(String nameIN, Integer priceIN, String descriptionIN,
+            String imageIN, Integer ageLimitIN, Integer discountIN, Integer inStockIN, Integer deviceIdIN, Integer platformIdIN) {
+        try {
+
+            if (!Game.checkNameUnique(nameIN)) {
+                return "Hibás név!";
+            } else if (!Game.checkName(nameIN)) {
+                return "Hibás név!";
+            } else if (!Game.checkPrice(priceIN)) {
+                return "Hibás ár!";
+            } else if (!Game.checkDescription(descriptionIN)) {
+                return "Hibás leírás!";
+            } else if (!Game.checkImageUnique(imageIN)) {
+                return "Hibás kép!";
+            } else if (!Game.checkImage(imageIN)) {
+                return "Hibás kép!";
+            } else if (!Game.checkAgeLimit(ageLimitIN)) {
+                return "Hibás korhatár!";
+            } else if (!Game.checkDiscount(discountIN)) {
+                return "Hibás akció!";
+            } else if (!Game.checkInStock(inStockIN)) {
+                return "Hibás mennyiségraktáron!";
+            } else if (Game.newGame(nameIN, priceIN, descriptionIN, imageIN, ageLimitIN, discountIN,
+                    inStockIN, deviceIdIN, platformIdIN)) {
+                return "Játék hozzáadva!";
+            } else {
+                return "Hiba a Játék hozzáadásánál!";
+            }
+        } catch (GameException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public static String updateGame(Integer idIN, String nameIN, Integer priceIN, String descriptionIN,
+            String imageIN, Integer ageLimitIN, Integer discountIN, Integer inStockIN, Integer deviceIdIN, Integer platformIdIN) {
+        try {
+            if (Game.game(idIN).isEmpty()) {
+                return "A játék nem létezik!";
+            } else if (!Game.checkNameUnique(nameIN)) {
+                return "Hibás név!";
+            } else if (!Game.checkName(nameIN)) {
+                return "Hibás név!";
+            } else if (!Game.checkPrice(priceIN)) {
+                return "Hibás ár!";
+            } else if (!Game.checkDescription(descriptionIN)) {
+                return "Hibás leírás!";
+            } else if (!Game.checkImageUnique(imageIN)) {
+                return "Hibás kép!";
+            } else if (!Game.checkImage(imageIN)) {
+                return "Hibás kép!";
+            } else if (!Game.checkAgeLimit(ageLimitIN)) {
+                return "Hibás korhatár!";
+            } else if (!Game.checkDiscount(discountIN)) {
+                return "Hibás akció!";
+            } else if (!Game.checkInStock(inStockIN)) {
+                return "Hibás mennyiségraktáron!";
+            } else if (Game.updateGame(idIN, nameIN, priceIN, descriptionIN, imageIN, ageLimitIN, discountIN,
+                    inStockIN, deviceIdIN, platformIdIN)) {
+                return "Játék frissítve!";
+            } else {
+                return "Hiba a Játék frissítésénél!";
+            }
+        } catch (GameException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public static String deleteGame(Integer idIN) {
+        try {
+            if (Game.game(idIN).isEmpty()) {
+                return "A játék nem létezik!";
+            } else if (Game.deleteGame(idIN)) {
+                return "Játék törölve!";
+            } else {
+                return "Hiba a Játék törlésénél!";
+            }
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
     }
 
 }
