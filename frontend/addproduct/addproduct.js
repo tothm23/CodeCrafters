50791c9b76fa1184a1280f0166c02d18270f769e
varadib02 <91455828+varadib02@users.noreply.github.com>
2024-03-26 15:27:44 +0100

    create kész

--- frontend/addproduct/addproduct.js
+++ frontend/addproduct/addproduct.js
@@ -6,7 +6,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Kinyerjük a bejelentkezési adatokat a localStorage-ból
     const logeduser_data = JSON.parse(localStorage.getItem("loged_userdata"));
     const logeduser_token = localStorage.getItem("loged_usertoken");
+
     let preview = document.getElementById("preview");
+    let save_btn = document.getElementById("save_btn");
+    let create_btn = document.getElementById("create_btn");
+
     let productName = document.getElementById("productName");
     let price = document.getElementById("price");
     let discount = document.getElementById("discount");
@@ -23,74 +27,87 @@ document.addEventListener('DOMContentLoaded', function () {
     let parameterek = new URL(document.location).searchParams;
     // Kiolvassa az id paramétert
     let id = parseInt(parameterek.get("id"));
-    let link = "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/game";
-    addEventListener('load', function () {
-        if (logeduser_data.id && logeduser_data.admin == 1) {} else {
-            // Ha nincs bejelentkezett felhasználó, valamilyen hiba kezelése vagy irányítás
-            console.error("Nincs bejelentkezett felhasználó");
-            create_btn.style.display = "none";
-        }
-    });
+    console.log(id);
+    if (isNaN(id)) {
+        save_btn.style.display = "none";
 
-    const requestOptions = {
-        method: "GET",
-        redirect: "follow"
-    };
 
-    fetch(`${link}/${id}`, requestOptions)
-        //a kapott adatokat json formátumban használjuk tovább
-        .then((response) => response.json())
-        .then((result) => {
-            console.log(result);
+    } else {
+        create_btn.style.display = "none";
 
-            data = result;
+        let link = "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/game";
 
-            console.log("Data: ", data);
-            preview.src = "../img/games/" + result.image;
-            
-            get_imageName = result.image;
-            console.log("Image: ",get_imageName);
-            preview.style.display = "block";
-            productName.value = result.gameName;
-            price.value = result.price;
-            discount.value = result.discount;
-            stock.value = result.inStock;
-            for (let i = 0; i < platform.length; i++) {
-                console.log(platform[i]);
-                if (platform[i].value == result.platformName) {
-                    console.log(platform[i].value);
-                    platform[i].selected = true;
-                    console.log(platform.selectedIndex);
-                    break;
-                }
+        addEventListener('load', function () {
+            if (logeduser_data.id && logeduser_data.admin == 1) {} else {
+                // Ha nincs bejelentkezett felhasználó, valamilyen hiba kezelése vagy irányítás
+                console.error("Nincs bejelentkezett felhasználó");
+                create_btn.style.display = "none";
             }
+        });
 
-            for (let i = 0; i < agelimit.length; i++) {
-                console.log(agelimit[i]);
-                if (agelimit[i].value == result.ageLimit) {
-                    console.log(agelimit[i].value);
-                    agelimit[i].selected = true;
-                    console.log(agelimit.selectedIndex);
-                    break;
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        fetch(`${link}/${id}`, requestOptions)
+            //a kapott adatokat json formátumban használjuk tovább
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result);
+
+                data = result;
+
+                console.log("Data: ", data);
+                preview.src = "../img/games/" + result.image;
+
+                get_imageName = result.image;
+                console.log("Image: ", get_imageName);
+                preview.style.display = "block";
+                productName.value = result.gameName;
+                price.value = result.price;
+                discount.value = result.discount;
+                stock.value = result.inStock;
+                for (let i = 0; i < platform.length; i++) {
+                    console.log(platform[i]);
+                    if (platform[i].value == result.platformName) {
+                        console.log(platform[i].value);
+                        platform[i].selected = true;
+                        console.log(platform.selectedIndex);
+                        break;
+                    }
                 }
-            }
 
-            for (let i = 0; i < device.length; i++) {
-                console.log(device[i]);
-                if (device[i].value == result.deviceName) {
-                    console.log(device[i].value);
-                    device[i].selected = true;
-                    console.log(device.selectedIndex);
-                    break;
+                for (let i = 0; i < agelimit.length; i++) {
+                    console.log(agelimit[i]);
+                    if (agelimit[i].value == result.ageLimit) {
+                        console.log(agelimit[i].value);
+                        agelimit[i].selected = true;
+                        console.log(agelimit.selectedIndex);
+                        break;
+                    }
                 }
-            }
 
-            desc.value = result.description;
+                for (let i = 0; i < device.length; i++) {
+                    console.log(device[i]);
+                    if (device[i].value == result.deviceName) {
+                        console.log(device[i].value);
+                        device[i].selected = true;
+                        console.log(device.selectedIndex);
+                        break;
+                    }
+                }
 
-        })
-        .catch((error) => console.error(error));
+                desc.value = result.description;
 
-    document.getElementById("save_btn").addEventListener("click", () => save(logeduser_token, id));
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    create_btn.addEventListener('click', () => create(logeduser_token));
+    save_btn.addEventListener("click", () => save(logeduser_token, id));
 
 });
 
@@ -113,17 +130,17 @@ function previewImage(event) {
     reader.readAsDataURL(uploadedFile);
 }
 
-function save(logeduser_token, id) {
+function save(logeduser_token) {
     let limit = document.getElementById("age");
     let selectedOption = limit.options[limit.selectedIndex];
     let selectedText = selectedOption.textContent;
     console.log("Selected age:", selectedText);
-    
+
     console.log(imageName);
     console.log(get_imageName);
-    if(imageName===undefined)imageName=get_imageName;
+    if (imageName === undefined) imageName = get_imageName;
     console.log(imageName);
-    
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     console.log(JSON.stringify(myHeaders));
@@ -150,6 +167,47 @@ function save(logeduser_token, id) {
     };
 
     fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/game/${id}`, requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+}
+
+function create(logeduser_token, id) {
+    let limit = document.getElementById("age");
+    let selectedOption = limit.options[limit.selectedIndex];
+    let selectedText = selectedOption.textContent;
+    console.log("Selected age:", selectedText);
+    
+    console.log(imageName);
+
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    console.log(JSON.stringify(myHeaders));
+    console.log(logeduser_token);
+    myHeaders.append("Authorization", `Bearer ${logeduser_token}`);
+    console.log(JSON.stringify(myHeaders));
+
+    const raw = JSON.stringify({
+        "name": productName.value,
+        "price": price.value,
+        "description": desc.value,
+        "image": imageName,
+        "ageLimit": selectedText,
+        "discount": discount.value,
+        "inStock": stock.value,
+        "deviceId": device.selectedIndex,
+        "platformId": platform.selectedIndex
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/game", requestOptions)
         .then((response) => response.text())
         .then((result) => console.log(result))
         .catch((error) => console.error(error));
