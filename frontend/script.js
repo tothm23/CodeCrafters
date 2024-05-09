9219b297913ed46f0ec2df827ddb380ba168197a
tothm23 <tothmilan224@gmail.com>
2024-05-09 19:02:31 +0200

    frontend mappa létrehozása
    Az összes frontendhez tartozó mappa és fájl a frontend mappába került

--- frontend/script.js
+++ frontend/script.js
@@ -1,83 +1,132 @@
-const bejelentkezettFelhasznalo = JSON.parse(localStorage.getItem("bejelentkezes"));
+const logged_user = JSON.parse(localStorage.getItem("loged_userdata"));
 const log_reg = document.getElementById("log_reg");
-const felhasznaloful = document.getElementById("felhasznalo-box");
+const userbox = document.getElementById("user-box");
+const loged_usertoken = localStorage.getItem("loged_usertoken");
 
-let kosarid = [];
-
-
-if(bejelentkezettFelhasznalo){
-  log_reg.display="none";
-  felhasznaloful.display="flex";
-  getkosar(kosarid);  
-}
-else{
-  log_reg.display="flex";
-  felhasznaloful.display="none";
-}
+let cart_id = [];
 
 // GET kérés
 fetch(
-    "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/fooldal"
+    "http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/mainPage"
   )
-  .then((valasz) => valasz.json())
-  .then((adat) => {
+  .then((response) => response.json())
+  .then((data) => {
     var carouselInner = document.getElementById("gameCarousel");
+    var bestseller_card = document.getElementById("bestseller_card");
+    var new_card = document.getElementById("new_card");
 
-    for (let i = 0; i < adat.length; i++) {
-      var akciosAr = Math.round(
-        adat[i].ar - (adat[i].ar / 100) * adat[i].akcio
+    for (let i = 0; i < data.length; i++) {
+      var salePrice = Math.round(
+        data[i].price - (data[i].price / 100) * data[i].discount
       );
-      carouselInner.innerHTML += `
+      if (data[i].type == "carousel") {
+        carouselInner.innerHTML += `
         <div class="carousel-item">
-          <div class="card flex-column flex-lg-row">
-            <a href="./jatek/jatek.html?id=${adat[i].id}"><img src="./kepek/jatekok/${
-              adat[i].kep
-            }" class="d-block w-100" alt="${adat[i].nev}">
-            </a>
-            <div class="card-body">
-              <h5 class="card-title">${adat[i].nev}</h5>
-              ${
-                adat[i].akcio > 0
-                  ? `<p class="card-text ar" style="text-decoration: line-through;">${adat[i].ar} Ft</p>`
-                  : `<p class="card-text ar">${adat[i].ar} Ft</p>`
-              }
-              ${
-                adat[i].akcio > 0
-                  ? `<p class="card-text akciosar">${akciosAr} Ft</p>`
-                  : ""
-              }
-              ${hozzaadasGomb(adat[i].id)}
-            </div>  
-          </div>
+        <div class="card d-flex flex-column flex-md-row">
+        <a class="d-block w-100" href="./game/game.html?id=${data[i].id}"><img src="./img/games/${
+          data[i].image
+        }" class="d-block w-100" alt="${data[i].name}">
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${data[i].name}</h5>
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text" style="text-decoration: line-through;">${data[i].price} Ft</p>`
+              : `<p class="card-text">${data[i].price} Ft</p>`
+          }
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text saleprice">${salePrice} Ft</p>`
+              : ""
+          }
+          ${add_btn(data[i].id)}
+        </div>
         </div>
       `;
-      // Kiválasztjuk az első carousel-item-et
-      var elsoCarouselElem = document.querySelector(".carousel-item");
+        // Kiválasztjuk az első carousel-item-et
+        var elsoCarouselElem = document.querySelector(".carousel-item");
 
-      // Hozzáadjuk az "active" osztályt
-      elsoCarouselElem.classList.add("active");
+        // Hozzáadjuk az "active" osztályt
+        elsoCarouselElem.classList.add("active");
+      }
+      if(data[i].type === "bestseller")
+      {
+        bestseller_card.innerHTML += `
+        <div class="card d-flex flex-column">
+        <a class="d-block w-100" href="./game/game.html?id=${data[i].id}"><img src="./img/games/${
+          data[i].image
+        }" class="d-block w-100" alt="${data[i].name}">
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${data[i].name}</h5>
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text" style="text-decoration: line-through;">${data[i].price} Ft</p>`
+              : `<p class="card-text">${data[i].price} Ft</p>`
+          }
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text saleprice">${salePrice} Ft</p>`
+              : ""
+          }
+          ${add_btn(data[i].id)}
+        </div>
+      `;
+      }
+      if(data[i].type === "new"){
+        new_card.innerHTML += `
+        <div class="card d-flex flex-column">
+        <a class="d-block w-100" href="./game/game.html?id=${data[i].id}"><img src="./img/games/${
+          data[i].image
+        }" class="d-block w-100" alt="${data[i].name}">
+        </a>
+        <div class="card-body">
+          <span class="badge bg-secondary">Új</span>
+          <h5 class="card-title">${data[i].name}</h5>
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text" style="text-decoration: line-through;">${data[i].price} Ft</p>`
+              : `<p class="card-text">${data[i].price} Ft</p>`
+          }
+          ${
+            data[i].discount > 0
+              ? `<p class="card-text saleprice">${salePrice} Ft</p>`
+              : ""
+          }
+          ${add_btn(data[i].id)}
+        </div>
+      `;
+      }
     }
   })
-  .catch((hiba) => alert(hiba));
+  .catch((error) => alert(error));
 
 //get kosár
-function getkosar(gombid) {
-  fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/kosar/${bejelentkezettFelhasznalo.id}`, {
-      method: 'GET'
-    })
+function getCart(btn_id) {
+  console.log(logged_user);
+  console.log(loged_usertoken);
+  const myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${loged_usertoken}`);
+        console.log(myHeaders);
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
+  fetch(`http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/basket`,requestOptions)
     .then(response => response.json())
     .then(result => {
       // Ellenőrizze, hogy a válasz tartalmazza-e a várt tulajdonságokat
-      console.log('Eredmény:', result);
+      //console.log('Eredmény:', result);
       for (let i = 0; i < result.length; i++) {
-        gombid[i] = result[i].jatekId;
-        console.log(gombid[i], result[i].jatekId);
-        console.log(kosarid[0], kosarid[1], kosarid[2]);
+        btn_id[i] = result[i].gameId;
+        console.log(btn_id[i], result[i].gameId);
+        console.log(cart_id[0], cart_id[1], cart_id[2]);
       }
     }).catch(error => console.error(error));
 }
 // vissza gomb
-let mybutton = document.getElementById("vissza-gomb");
+let mybutton = document.getElementById("scroll_Up");
 
 window.onscroll = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -87,106 +136,131 @@ window.onscroll = () => {
   }
 };
 
-function felgorget() {
+function scroll_Up() {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
 //token
-function parseJwt (token) {
+function parseJwt(token) {
   var base64Url = token.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
   }).join(''));
 
   return JSON.parse(jsonPayload);
 }
 
-function hozzaadasGomb(id) {
+function add_btn(id) {
   var vanilyen_ID = false;
-  if (bejelentkezettFelhasznalo) {
-    for (let i = 0; i < kosarid.length; i++) {
-      if (kosarid[i] === id) {
+  if (logged_user) {
+    for (let i = 0; i < cart_id.length; i++) {
+      if (cart_id[i] === id) {
         vanilyen_ID = true;
+        console.log(cart_id[i]);
       }
     }
     if (vanilyen_ID) {
-      return `<input id="hozzadas" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" disabled value="Kosárban" />`;
+      return `<input id="add_btn" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" disabled value="Kosárban" />`;
     } else {
-      return `<input id="hozzadas" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás a kosárhoz" />`;
+      return `<input id="add_btn" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" value="Hozzáadás" />`;
 
     }
   } else {
-    return `<input id="hozzadas" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" disabled value="Hozzáadás a kosárhoz" />`;
+    return `<input id="add_btn" data-jatek-id="${id}" class="my-2 p-2 btn btn-success fs-5" type="button" disabled value="Hozzáadás" />`;
   }
-
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  const keresoInput = document.querySelector('.navbar input[type="search"]');
+  const search_Input = document.querySelector('.navbar input[type="search"]');
   const form = document.querySelector(".navbar form");
-  const aktualisPath = window.location.pathname;
-  const kijelentkezés_gomb = document.getElementById("felhasznalo_kilep");
-  const beallitasok_gomb = document.getElementById("beallitasok");
-  const fnev = document.getElementById("felhasznalo");
-  //kijelentkezés essetén
-  kijelentkezés_gomb.addEventListener("click", function () {
-    localStorage.removeItem("bejelentkezes");
-    log_reg.style.display = "flex";
-    felhasznaloful.style.display = "none";
-  });
-  beallitasok_gomb.addEventListener("click", function () {
-    if (aktualisPath == "/frontend/index.html") {
-      // Most már tovább navigálhatsz az új oldalra
-      window.open("./felhasznalo/felhasznalo.html");
-    } else {
-      window.open("../felhasznalo/felhasznalo.html");
+  const actual_path = window.location.pathname;
+  const logout = document.getElementById("logout");
+  const settings_btn = document.getElementById("settings");
+  const cart_btn = document.getElementById("cart");
+  const admin_btn = document.getElementById("admin");
+  const user_name = document.getElementById("username");
+  const email = document.getElementById("email");
+  const login = document.getElementById("login");
+  const reg = document.getElementById("registration");
+
+  if (logged_user) {
+    log_reg.display = "none";
+    userbox.display = "flex";
+    if(logged_user.admin!=1){
+      admin_btn.style.display = "none";
     }
+    getCart(cart_id);
+  } else {
+    log_reg.display = "flex";
+    userbox.display = "none";
+  }
+
+  //kijelentkezés essetén
+  logout.addEventListener("click", function () {
+    localStorage.removeItem("loged_userdata");
+    log_reg.style.display = "flex";
+    userbox.style.display = "none";
+  });
+  settings_btn.addEventListener("click", function () {
+      window.location.href = "../user/user.html";
+  });
+  cart_btn.addEventListener("click", function () {
+      window.location.href = "../cart/cart.html";
+  });
+  admin_btn.addEventListener("click", function () {
+      window.location.href = "../admin/admin.html";
+  });
+  reg.addEventListener("click", function () {
+      window.location.href = "../registration/registration.html";
+  });
+  login.addEventListener("click", function () {
+      window.location.href = "../login/login.html";
   });
   // Eseményfigyelők hozzáadása a keresőhöz és az ármezőkhöz
   form.addEventListener("submit", function (esemeny) {
     esemeny.preventDefault();
 
     // A keresőszöveg tárolása a localStorage-ban
-    localStorage.setItem('keresesSzoveg', keresoInput.value.toLowerCase());
+    localStorage.setItem('searched_text', search_Input.value.toLowerCase());
 
-    if (aktualisPath == "/frontend/index.html") {
+    if (actual_path == "index.html") {
       // Tovább navigálunk az új oldalra
-      window.location.href = "./termekek/termekek.html";
-    } else if (aktualisPath == "/frontend/jatek/jatek.html") {
-      window.location.href = "../termekek/termekek.html";
+      window.location.href = "./products/products.html";
+    } else if (actual_path == "/game/game.html") {
+      window.location.href = "../products/products.html";
     } else {
       // Egyéb esetek kezelése (opcionális)
     }
   });
 
-
-  if (bejelentkezettFelhasznalo) {
-    console.log(bejelentkezettFelhasznalo.id);
+  if (logged_user) {
+    console.log(logged_user.id);
     log_reg.style.display = "none";
-    felhasznaloful.style.display = "flex";
-    fnev.innerHTML = "";
-    fnev.innerHTML += bejelentkezettFelhasznalo.felhasznaloNev;
+    userbox.style.display = "flex";
+    user_name.innerHTML += logged_user.userName;
+    email.innerHTML += logged_user.email;
   } else {
     log_reg.style.display = "flex";
-    felhasznaloful.style.display = "none";
+    userbox.style.display = "none";
   }
 
   document.addEventListener("click", function (event) {
-    if (event.target && event.target.id === "hozzadas") {
-      //ha hozzáadjuk a kosárhoz akkor disbaled és a value megváltozik kosárbanra
+    if (event.target && event.target.id === "add_btn") {
+      //ha hozzáadjuk a kosárhoz akkor disabled és a value megváltozik kosárban-ra
       event.target.value = "Kosárban";
       event.target.disabled = true;
-      const jatekId = event.target.getAttribute("data-jatek-id");
+      const gameId = event.target.getAttribute("data-jatek-id");
 
-      if (jatekId)
+      if (gameId)
         var myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Authorization", `Bearer ${loged_usertoken}`);
       var raw = JSON.stringify({
-        jatekId: jatekId,
-        felhasznaloId: bejelentkezettFelhasznalo.id
+        gameId: gameId,
+        userId: logged_user.id
       });
 
       console.log(raw);
@@ -198,7 +272,7 @@ document.addEventListener("DOMContentLoaded", function () {
         redirect: 'follow'
       };
 
-      fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/kosar", requestOptions)
+      fetch("http://localhost:8080/CodeCraftersWebshop-1.0-SNAPSHOT/webresources/basket", requestOptions)
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
